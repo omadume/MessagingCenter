787fc42830837905dec5cb91c6b2392ba06bb3b8
--local <madumeolivia@gmail.com>
2024-04-10 19:39:46 +0100

    adding further tests to passenger import feature spec

--- spec/features/passengers_import_spec.rb
+++ spec/features/passengers_import_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.feature "PassengersImport", type: :feature do
 
     let(:csvFile) { Rails.root.join('spec/fixtures/passengers.csv') }
+    let!(:passenger) { Passenger.create!(name: "Jeff Rustic", email: "jeffrustic@gmail.com", gender: "x", date_of_birth: "1997-04-10", status: "created", passenger_id: "001") }
 
     describe "User imports a CSV file" do
         it "imports successfully with a valid CSV file" do
@@ -14,7 +15,6 @@ RSpec.feature "PassengersImport", type: :feature do
             
             expect(page).to have_current_path(passengers_path)
             expect(page).to have_content("Passenger data imported successfully")
-            expect(page).to have_content("Jeff Rustic")
             expect(initial_passenger_count).to eq(0)
             expect(new_passenger_count > initial_passenger_count)
         end
@@ -28,6 +28,20 @@ RSpec.feature "PassengersImport", type: :feature do
             expect(page).to have_current_path(import_passengers_path)
             expect(initial_passenger_count).to eq(0)
             expect(new_passenger_count == initial_passenger_count)
+        end
+
+        it "does not import duplicates - identified by passenger email" do
+            initial_passenger_count = Passenger.count
+            initial_passengers = Passenger.all
+            visit root_path
+            attach_file('file', csvFile)
+            click_button "Import CSV"
+            new_passenger_count = Passenger.count
+
+            expect(initial_passenger_count).to eq(1)
+            expect(initial_passengers).to include(passenger)
+            expect(new_passenger_count > initial_passenger_count)
+            expect(Passenger.where(email: "jeffrustic@gmail.com").count).to eq(1)
         end
     end
 end
