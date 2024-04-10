c9d52494fcec83f10570d7c2b08a6ac48b5665a9
--local <madumeolivia@gmail.com>
2024-04-10 17:47:00 +0100

    adding model specs

---
+++ spec/models/package_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Package, type: :model do
+    
+    let(:package) { Package.new(name: "Package 1")}
+
+    it "is valid with all required attributes" do
+        expect(package).to be_valid
+    end
+
+    it "is not valid if missing a required attribute" do
+        package.name = ""
+        expect(package).not_to be_valid
+    end
+
+    it "is not valid with an unknown attribute" do
+        expect{package.assign_attributes(random_attribute: "random_value")}.to raise_error(ActiveModel::UnknownAttributeError)
+    end
+
+    it "is valid with multiple passengers" do
+        # Persisting objects to database to allow for checking validity of relational associations
+        package.save!
+        passenger1 = Passenger.create!(name: "Jenny", email: "jenny@email.com", gender: "x", date_of_birth: "1997-04-10", status: "created", passenger_id: "001", package: package)
+        passenger2 = Passenger.create!(name: "John", email: "john@email.com", gender: "x", date_of_birth: "1997-04-16", status: "created", passenger_id: "002", package: package)
+        expect(package.passengers).to include(passenger1, passenger2)
+    end
+end
---
+++ spec/models/passenger_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Passenger, type: :model do
+
+    let(:passenger) { Passenger.new(name: "Jenny", email: "jenny@email.com", gender: "x", date_of_birth: "1997-04-10", status: "created", passenger_id: "001") }
+    let(:package) { Package.new(name: "Package 1")}
+
+    it "is valid with all required attributes" do
+        expect(passenger).to be_valid
+    end
+
+    it "is not valid if missing a required attribute" do
+        passenger.name = ""
+        expect(passenger).not_to be_valid
+    end
+
+    it "is not valid with an unknown attribute" do
+        expect{ passenger.assign_attributes(random_attribute: "random_value") }.to raise_error(ActiveModel::UnknownAttributeError)
+    end
+
+    it "is valid with one package (not required)" do
+        passenger.package = package
+        expect(passenger).to be_valid
+    end
+
+    it "is not valid with multiple packages" do
+        expect{ passenger.package = [] }.to raise_error(ActiveRecord::AssociationTypeMismatch)
+    end
+
+    it "is valid with array of messages (not required)" do
+        passenger.messages = []
+        expect(passenger).to be_valid
+    end
+
+end
