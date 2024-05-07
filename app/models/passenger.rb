d2c0a718517f825bfefd41f60c024d232f4bf1d9
--local <madumeolivia@gmail.com>
2024-05-07 17:49:45 +0100

    amending indentation to 2 spaces

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,46 +1,46 @@
 require 'csv'
 
 class Passenger < ApplicationRecord
-    belongs_to :package, optional: true
-    before_validation :downcase_email # Ensure any emails already existing in the DB are lowercase for case-insensitive matching
-    validates_presence_of :name, :email, :gender, :date_of_birth, :status, :passenger_id # Assumption that a Passenger may not have a Package upon account creation
-    
-    def self.importCsv(file)
-        begin
-            CSV.foreach(file.path, headers: true){ |row|
-                # Convert CSV row data to hash using column headers, and transform to match model naming conventions
-                passenger_attributes = row.to_hash.transform_keys{ |key| key.downcase.gsub(' ', '_') }
+  belongs_to :package, optional: true
+  before_validation :downcase_email # Ensure any emails already existing in the DB are lowercase for case-insensitive matching
+  validates_presence_of :name, :email, :gender, :date_of_birth, :status, :passenger_id # Assumption that a Passenger may not have a Package upon account creation
 
-                # Remove Package column data and use to create Package object
-                package_name = passenger_attributes.delete("package")
-                package = Package.find_or_create_by!(name: package_name) if package_name.present?
+  def self.import_csv(file)
+    begin
+      CSV.foreach(file.path, headers: true){ |row|
+        # Convert CSV row data to hash using column headers, and transform to match model naming conventions
+        passenger_attributes = row.to_hash.transform_keys{ |key| key.downcase.gsub(' ', '_') }
 
-                # Extract and map certain csv column data separately to avoid name mismatch with model
-                full_name = passenger_attributes.delete("full_name")
-                passenger_attributes["name"] = full_name if full_name.present?
+        # Remove Package column data and use to create Package object
+        package_name = passenger_attributes.delete("package")
+        package = Package.find_or_create_by!(name: package_name) if package_name.present?
 
-                # Tranform email to lowercase for case-insensitive matching
-                passenger_attributes["email"] = passenger_attributes["email"].downcase if passenger_attributes["email"].present?
+        # Extract and map certain csv column data separately to avoid name mismatch with model
+        full_name = passenger_attributes.delete("full_name")
+        passenger_attributes["name"] = full_name if full_name.present?
 
-                # Create or update Passenger object, with a Package
-                passenger = Passenger.find_or_initialize_by(email: passenger_attributes["email"])
-                passenger.assign_attributes(passenger_attributes)
-                passenger.package = package if package.present?
-                passenger.save!
-            }
-            return true
-        rescue ActiveRecord::RecordInvalid => e
-            logger.error "Failed to import CSV: #{ e.message }"
-            return false
-        end
+        # Tranform email to lowercase for case-insensitive matching
+        passenger_attributes["email"] = passenger_attributes["email"].downcase if passenger_attributes["email"].present?
+
+        # Create or update Passenger object, with a Package
+        passenger = Passenger.find_or_initialize_by(email: passenger_attributes["email"])
+        passenger.assign_attributes(passenger_attributes)
+        passenger.package = package if package.present?
+        passenger.save!
+      }
+      return true
+    rescue ActiveRecord::RecordInvalid => e
+      logger.error "Failed to import CSV: #{ e.message }"
+      return false
     end
+  end
 
-    def age
-        Date.today.year - date_of_birth.year
+  def age
+    Date.today.year - date_of_birth.year
+  end
+
+  private
+    def downcase_email
+      self.email = email.downcase if email.present?
     end
-
-    private
-        def downcase_email
-            self.email = email.downcase if email.present?
-        end
 end
